9e4ba54dbdca9e05e3b990147252e4bec5e0d923
Vasily93 <markovavasily@gmail.com>
2021-08-27 18:49:46 -0700

    init setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Game from './Game';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>BlackJack with React</h1>
+      <Game/>
+    </div>
+  );
+}
+
+export default App;
